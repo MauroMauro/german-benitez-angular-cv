597296f30647cd791ddfb66faf5f753a82f52711
MauroMauro <blur.benitez@gmail.com>
2021-05-24 16:38:50 -0300

    Update may-21

--- src/app/componentes/portfolio/portfolio.component.ts
+++ src/app/componentes/portfolio/portfolio.component.ts
@@ -10,7 +10,43 @@ export class PortfolioComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }  
+
+  } //fin ngOnInit
+
+  filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("column");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+      this.w3RemoveClass(x[i], "show");
+      if (x[i].className.indexOf(c) > -1) this.w3AddClass(x[i], "show");
+    }
+  }
+
+  w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+    }
+  }
+
+  w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+
+
+
+
 
 
 }
